3200a0f8e5c0ee98cc6f102ecf5610bc8944b593
Scott Johnson <jaywir3@gmail.com>
2016-10-07 10:49:27 -0500

    Add a task for removing items from the local maven repository.
    Fixes #14.

--- gruel/src/main/groovy/com/glasstowerstudios/gruel/GruelPlugin.groovy
+++ gruel/src/main/groovy/com/glasstowerstudios/gruel/GruelPlugin.groovy
@@ -3,7 +3,10 @@ package com.glasstowerstudios.gruel
 import com.glasstowerstudios.gruel.tasks.BumpVersionTask
 import com.glasstowerstudios.gruel.extensions.HipChatExtension
 import com.glasstowerstudios.gruel.extensions.GruelExtension
+import com.glasstowerstudios.gruel.tasks.UninstallTask
 
+import org.gradle.api.Task;
+import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -15,6 +18,11 @@ public class GruelPlugin implements Plugin<Project> {
     aProject.extensions.create("hipchat", HipChatExtension);
 
     aProject.task('bumpVersion', type: BumpVersionTask, description: 'Bumps the version number of the current release.', group: 'Management') << {
+    }
+
+    // We only want to add this task if the project is a library (android or JAR)
+    // Perhaps we can detect this if it has a maven publishing task?
+    aProject.task('uninstall', type: UninstallTask, description: "Uninstall any currently installed versions of '${aProject.name}' from the local maven repository") << {
     }
 
     aProject.afterEvaluate {
